75a57d5aa0d39fc79a31aaf12f191bf976428ec6
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-10-09 12:12:11 +0600

    System now works.

--- Floor_Tiling/Assets/Scripts/Floor.cs
+++ Floor_Tiling/Assets/Scripts/Floor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,4 +7,12 @@ public class Floor : MonoBehaviour
     [SerializeField] private List<Transform> spawnPositions;
 
     public List<Transform> SpawnPositions => spawnPositions;
+
+    private void OnTriggerEnter(Collider other){
+        // if (other.CompareTag("Player")){
+            FloorManager.Instance.UpdateFloors(transform);
+        // }
+
+        // Debug.Log("found something");
+    }
 }
--- Floor_Tiling/Assets/Scripts/FloorManager.cs
+++ Floor_Tiling/Assets/Scripts/FloorManager.cs
@@ -20,36 +20,36 @@ public class FloorManager : MonoBehaviour{
         var floorScript = floor.GetComponent<Floor>();
         _floorList.Add(floorScript);
         
-        var newFloors =PopulateFloor();
-        _floorList.AddRange(newFloors);
-        newFloors = PopulateFloor();
-        _floorList.AddRange(newFloors);
-        newFloors = PopulateFloor();
-        _floorList.AddRange(newFloors);
-        newFloors = PopulateFloor();
-        _floorList.AddRange(newFloors);
-        // PopulateFloor();
+        while (true){
+            var newFloors =PopulateFloor(transform ,out var continueLoop);
+            _floorList.AddRange(newFloors);
+            if (continueLoop == false){
+                break;
+            }
+        }
     }
 
-    private List<Floor> PopulateFloor(){
+    private List<Floor> PopulateFloor(Transform center,out bool continueLoop){
         var newTempList = new List<Floor>();
         var length = _floorList.Count;
+        continueLoop = false;
         for (int i = 0; i < length; i++){
             var floor = _floorList[i];
             var spawnPosList = floor.SpawnPositions;
-
             
             foreach (var tra in spawnPosList){
                 if (CheckEmptyPosition(tra, _floorList) && CheckEmptyPosition(tra, newTempList)){
-                    Debug.Log("Empty spot found");
-                    if (Vector3.Distance(transform.position, tra.position) < spawnRadius){
+                    // Debug.Log("Empty spot found");
+                    if (Vector3.Distance(center.position, tra.position) < spawnRadius){
+                        Debug.Log("Distance is: " + Vector3.Distance(transform.position, tra.position));
                         var newFloor = Instantiate(floorPrefab, transform);
                         newFloor.transform.position = tra.position;
                         var floorScript = newFloor.GetComponent<Floor>();
                         newTempList.Add(floorScript);
+                        continueLoop = true;
                     }
                 }
-                Debug.Log("Empty spot not found");
+                // Debug.Log("Empty spot not found");
             }
         }
 
@@ -64,5 +64,36 @@ public class FloorManager : MonoBehaviour{
         }
 
         return true;
+    }
+
+    private List<Floor> GetOutOfRadiusFloors(Transform center){
+        var tempList = new List<Floor>();
+        foreach (var floor in _floorList){
+            if (Vector3.Distance(center.position, floor.transform.position) > spawnRadius){
+                tempList.Add(floor);
+            }
+        }
+        return tempList;
+    }
+    
+    private void DeleteFloors(List<Floor> floors){
+        
+        foreach (var floor in floors){
+            _floorList.Remove(floor);
+            Destroy(floor.gameObject);
+        }
+    }
+
+    public void UpdateFloors(Transform center){
+        var floorsToDelete = GetOutOfRadiusFloors(center);
+        DeleteFloors(floorsToDelete);
+        
+        while (true){
+            var newFloors =PopulateFloor(center ,out var continueLoop);
+            _floorList.AddRange(newFloors);
+            if (continueLoop == false){
+                break;
+            }
+        }
     }
 }
